da695b801257be6cf845d9ae79fa7ff03f45db3a
Kristyn Le <kristynle0608@gmail.com>
2026-01-15 12:38:18 -0400

    add some functionalities to easy quiz

--- src/components/EasyQuiz.jsx
+++ src/components/EasyQuiz.jsx
@@ -1,4 +1,40 @@
 import { useState } from 'react'
+import './EasyQuiz.css'
+
+const question1Options = [
+        { id: 1, option: "2019", checked: false, correct: false },
+        { id: 2, option: "2020", checked: false, correct: true },
+        { id: 3, option: "2021", checked: false, correct: false },
+        { id: 4, option: "2022", checked: false, correct: false }
+    ]
+
+    const question2Options = [
+        { id: 1, option: "First-person Shooter", checked: false, correct: false },
+        { id: 2, option: "Third-person Shooter", checked: false, correct: true },
+        { id: 3, option: "Top-Down/Isometric Shooter", checked: false, correct: false },
+        { id: 4, option: "Rail Shooter", checked: false, correct: false }
+    ]
+
+    const question3Options = [
+        { id: 1, option: "Brimstone", checked: false, correct: false },
+        { id: 2, option: "Jett", checked: false, correct: false },
+        { id: 3, option: "Sova", checked: false, correct: false },
+        { id: 4, option: "Reyna", checked: false, correct: true }
+    ]
+
+    const question4Options = [
+        { id: 1, option: "3", checked: false, correct: false },
+        { id: 2, option: "4", checked: false, correct: false },
+        { id: 3, option: "5", checked: false, correct: true },
+        { id: 4, option: "6", checked: false, correct: false }
+    ]
+
+    const question5Options = [
+        { id: 1, option: "Rockstar Games", checked: false, correct: false },
+        { id: 2, option: "Riot Games", checked: false, correct: true },
+        { id: 3, option: "Valve", checked: false, correct: false },
+        { id: 4, option: "Epic Games", checked: false, correct: false }
+    ]
 
 function EasyQuiz() {
     const [isDisabled1, setIsDisabled1] = useState(false);
@@ -6,58 +42,54 @@ function EasyQuiz() {
     
     const [isDisabled2, setIsDisabled2] = useState(false);
     const [selectedValue2, setSelectedValue2] = useState("");
-    // const [state3, setState3] = useState(false);
-    // const [state4, setState4] = useState(false);
-    // const [state5, setState5] = useState(false);
 
-    const handleChange1 = (e) => {
+    const [isDisabled3, setIsDisabled3] = useState(false);
+    const [selectedValue3, setSelectedValue3] = useState("");
+
+    const [isDisabled4, setIsDisabled4] = useState(false);
+    const [selectedValue4, setSelectedValue4] = useState("")
+
+    const [isDisabled5, setIsDisabled5] = useState(false);
+    const [selectedValue5, setSelectedValue5] = useState("")
+
+    function handleChange1(e, item) {
         setSelectedValue1(e.target.value);
         setIsDisabled1(true);
+
+        item.checked = true;
     }
 
-    const handleChange2 = (e) => {
+    function handleChange2(e, item) {
         setSelectedValue2(e.target.value);
         setIsDisabled2(true);
+
+        item.checked = true;
     }
- 
-    const question1Options = [
-        { id: 1, option: "2019" },
-        { id: 2, option: "2020" },
-        { id: 3, option: "2021" },
-        { id: 4, option: "2022" }
-    ]
 
-    const question2Options = [
-        { id: 1, option: "First-person Shooter" },
-        { id: 2, option: "Third-person Shooter" },
-        { id: 3, option: "Top-Down/Isometric Shooter" },
-        { id: 4, option: "Rail Shooter" }
-    ]
+    function handleChange3(e, item) {
+        setSelectedValue3(e.target.value);
+        setIsDisabled3(true);
 
-    const question3Options = [
-        { id: 1, option: "Brimstone" },
-        { id: 2, option: "Jett" },
-        { id: 3, option: "Sova" },
-        { id: 4, option: "Reyna" }
-    ]
+        item.checked = true;
+    }
 
-    const question4Options = [
-        { id: 1, option: 3 },
-        { id: 2, option: 4 },
-        { id: 3, option: 5 },
-        { id: 4, option: 6 }
-    ]
+    function handleChange4(e, item) {
+        setSelectedValue4(e.target.value);
+        setIsDisabled4(true);
 
-    const question5Options = [
-        { id: 1, option: "Rockstar Games" },
-        { id: 2, option: "Riot Games" },
-        { id: 3, option: "Valve" },
-        { id: 4, option: "Epic Games" }
-    ]
+        item.checked = true;
+    }
+
+    function handleChange5(e, item) {
+        setSelectedValue5(e.target.value);
+        setIsDisabled5(true);
+
+        item.checked = true;
+    }
 
     return (
         <>
-            <p>What year did Valorant come out?</p>
+            <p>What year was Valorant officially released?</p>
             {question1Options.map((option) => (
                 <>
                     <input 
@@ -67,15 +99,28 @@ function EasyQuiz() {
                         name="year-option" 
                         value={option.option} 
                         checked={selectedValue1 === option.option}
-                        onChange={handleChange1}
+                        onChange={(e) => handleChange1(e, option)}
                         disabled={isDisabled1}
                     >
                     </input>
-                    <label htmlFor={option.option}>{option.option}</label><br></br>
+                    <label 
+                        htmlFor={option.option}
+                        className={
+                            option.checked && option.correct ? "label-correct" 
+                            : option.checked && option.correct === false ? "label-incorrect" : ""
+                        }
+                    >
+                        {option.option}
+                    </label><br></br>
+
+                    <div className={option.checked && option.correct ? "p-checked" : "p-unchecked"}>
+                        ✅ Valorant was released on <b>June 2, 2020</b>.
+                    </div>
+                    <div className={option.checked && option.correct === false ? "p-checked" : "p-unchecked"}>
+                        ❌ Valorant was released on <b>June 2, 2020</b>.
+                    </div>
                 </>
             ))}
-
-            {/* Use useEffect to show which one is correct or wrong */}
 
             <p>What type of shooter game is Valorant (by perspective)?</p>
             {question2Options.map((option) => (
@@ -87,35 +132,125 @@ function EasyQuiz() {
                         name="type-option" 
                         value={option.option}
                         checked={selectedValue2 === option.option}
-                        onChange={handleChange2}
+                        onChange={(e) => handleChange2(e, option)}
                         disabled={isDisabled2}
                     >
                     </input>
-                    <label for={option.option}>{option.option}</label><br></br>
+                    <label 
+                        htmlFor={option.option}
+                        className={
+                            option.checked && option.correct ? "label-correct" 
+                            : option.checked && option.correct === false ? "label-incorrect" : ""
+                        }
+                    >   
+                        {option.option}
+                    </label><br></br>
+
+                    <div className={option.checked && option.correct ? "p-checked" : "p-unchecked"}>
+                        ✅ Valorant is a <b>third-person shooter</b> game.
+                    </div>
+                    <div className={option.checked && option.correct === false ? "p-checked" : "p-unchecked"}>
+                        ❌ Valorant is a <b>third-person shooter</b> game, not a <i>{option.option.toLowerCase()}</i>.
+                    </div>
                 </>
             ))}
 
             <p>Which of the following agents is NOT a part of the original lineup?</p>
             {question3Options.map((option) => (
                 <>
-                    <input type="radio" key={option.id} id={option.option} name="agent-option" value={option.option}></input>
-                    <label for={option.option}>{option.option}</label><br></br>
+                    <input 
+                        type="radio" 
+                        key={option.id} 
+                        id={option.option} 
+                        name="agent-option" 
+                        value={option.option}
+                        checked={selectedValue3 === option.option}
+                        onChange={(e) => handleChange3(e, option)}
+                        disabled={isDisabled3}
+                    >
+                    </input>
+                    <label 
+                        htmlFor={option.option}
+                        className={
+                            option.checked && option.correct ? "label-correct" 
+                            : option.checked && option.correct === false ? "label-incorrect" : ""
+                        }
+                    >
+                        {option.option}
+                    </label><br></br>
+
+                    <div className={option.checked && option.correct ? "p-checked" : "p-unchecked"}>
+                        ✅ Reyna is <b>not</b> a part of the original lineup.
+                    </div>
+                    <div className={option.checked && option.correct === false ? "p-checked" : "p-unchecked"}>
+                        ❌ <i><b>{option.option}</b></i> is a part of the original lineup; <b>Reyna</b> is the correct answer.
+                    </div>
                 </>
             ))}
 
             <p>What is the maximum number of players that a Valorant team can have?</p>
             {question4Options.map((option) => (
                 <>
-                    <input type="radio" key={option.id} id={option.option} name="team-option" value={option.option}></input>
-                    <label for={option.option}>{option.option}</label><br></br>
+                    <input 
+                        type="radio" 
+                        key={option.id} 
+                        id={option.option} 
+                        name="team-option" 
+                        value={option.option}
+                        checked={selectedValue4 === option.option}
+                        onChange={(e) => handleChange4(e, option)}
+                        disabled={isDisabled4}
+                    >
+                    </input>
+                    <label 
+                        htmlFor={option.option}
+                        className={
+                            option.checked && option.correct ? "label-correct" 
+                            : option.checked && option.correct === false ? "label-incorrect" : ""
+                        }
+                    >
+                        {option.option}
+                    </label><br></br>
+
+                    <div className={option.checked && option.correct ? "p-checked" : "p-unchecked"}>
+                        ✅ A Valorant team can have at most <b>5</b> players.
+                    </div>
+                    <div className={option.checked && option.correct === false ? "p-checked" : "p-unchecked"}>
+                        ❌ A Valorant team can have at most <b>5</b> players.
+                    </div>
                 </>
             ))}
 
             <p>Which game company created Valorant?</p>
             {question5Options.map((option) => (
                 <>
-                    <input type="radio" key={option.id} id={option.option} name="company-option" value={option.option}></input>
-                    <label for={option.option}>{option.option}</label><br></br>
+                    <input 
+                        type="radio" 
+                        key={option.id} 
+                        id={option.option} 
+                        name="company-option" 
+                        value={option.option}
+                        checked={selectedValue5 === option.option}
+                        onChange={(e) => handleChange5(e, option)}
+                        disabled={isDisabled5}
+                    >
+                    </input>
+                    <label 
+                        htmlFor={option.option}
+                        className={
+                            option.checked && option.correct ? "label-correct" 
+                            : option.checked && option.correct === false ? "label-incorrect" : ""
+                        }
+                    >
+                        {option.option}
+                    </label><br></br>
+
+                    <div className={option.checked && option.correct ? "p-checked" : "p-unchecked"}>
+                        ✅ <b>Riot Games</b>, whom also created <i>League of Legends</i>, released Valorant.
+                    </div>
+                    <div className={option.checked && option.correct === false ? "p-checked" : "p-unchecked"}>
+                        ❌ <i><b>{option.option}</b></i> may have created some popular shooting games, but <b>Riot Games</b> is the correct answer.
+                    </div>
                 </>
             ))}
         </>
